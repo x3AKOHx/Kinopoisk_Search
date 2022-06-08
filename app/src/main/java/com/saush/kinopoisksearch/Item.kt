7831295313cc62a176f6e6e51907f82b340bfbd4
Saushev Aleksandr <aleksandr_saushev@epam.com>
2022-06-08 16:31:25 +0300

    Alert dialogs for genre and countries added. slider for year pick added

--- app/src/main/java/com/saush/kinopoisksearch/Item.kt
+++
@@ -1,3 +0,0 @@
-package com.saush.kinopoisksearch
-
-data class Item(var name: String, var value: Boolean)
--- app/src/main/java/com/saush/kinopoisksearch/MainActivity.kt
+++ app/src/main/java/com/saush/kinopoisksearch/MainActivity.kt
@@ -2,18 +2,108 @@ package com.saush.kinopoisksearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import com.saush.kinopoisksearch.databinding.ActivityMainBinding
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.slider.RangeSlider
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var binding: ActivityMainBinding
+    private lateinit var filmGenre: TextView
+    private lateinit var country: TextView
+
+    private val genres = arrayOf("Боевик", "Драма", "Комедия", "Триллер")
+    private val countries = arrayOf("Россия", "США", "Англия", "Индия")
+
+    private val checkedGenres = mutableListOf<Boolean>()
+    private val checkedCountries = mutableListOf<Boolean>()
+
+    private val selectedGenres = ArrayList<String>()
+    private val selectedCountries = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)*/
+        //set listeners for genre and country fields
+        filmGenre = findViewById(R.id.film_genre)
+        country = findViewById(R.id.country)
+
+        filmGenre.setOnClickListener {
+            if (checkedGenres.isEmpty()){
+                repeat(genres.count()) {checkedGenres.add(false)}
+            }
+
+            AlertDialog.Builder(this)
+                .setTitle("Выберите жанр")
+                .setCancelable(false)
+                .setMultiChoiceItems(genres, checkedGenres.toBooleanArray()) {_, i, isChecked ->
+                    if (isChecked)
+                        selectedGenres.add(genres[i])
+                    else
+                        selectedGenres.remove(genres[i])
+
+                    checkedGenres[i] = isChecked
+                }
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    if (selectedGenres.size > 0)
+                    filmGenre.text = selectedGenres.joinToString(", ")
+                    else filmGenre.text = ""
+                }
+                .setNegativeButton("Cancel") {i, _, ->
+                    i.dismiss()
+                }
+                .setNeutralButton("Clear All") { _, _ ->
+                    selectedGenres.clear()
+                    checkedGenres.clear()
+                    filmGenre.text = ""
+                }
+                .show()
+        }
+        country.setOnClickListener {
+            if (checkedCountries.isEmpty()){
+                repeat(countries.count()) {checkedCountries.add(false)}
+            }
+
+            AlertDialog.Builder(this)
+                .setTitle("Выберите страну")
+                .setCancelable(false)
+                .setMultiChoiceItems(countries, checkedCountries.toBooleanArray()) {_, i, isChecked ->
+                    if (isChecked)
+                        selectedCountries.add(countries[i])
+                    else
+                        selectedCountries.remove(countries[i])
+
+                    checkedCountries[i] = isChecked
+                }
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    if (selectedCountries.size > 0)
+                        country.text = selectedCountries.joinToString(", ")
+                    else country.text = ""
+                }
+                .setNegativeButton("Cancel") {i, _, ->
+                    i.dismiss()
+                }
+                .setNeutralButton("Clear All") { _, _ ->
+                    selectedCountries.clear()
+                    checkedCountries.clear()
+                    country.text = ""
+                }
+                .show()
+        }
+
+        //set sliders for film year and rating picks
+        val filmYear: TextView = findViewById(R.id.year_pick)
+
+        val discreteRangeSlider: RangeSlider = findViewById(R.id.year_range)
+        discreteRangeSlider.addOnChangeListener { slider, _, _ ->
+            val from = slider.values[0].toInt()
+            val to = slider.values[1].toInt()
+            val range = if (from == to) from.toString()
+                        else if (from == 1930 && to == 2022) "Любой"
+                        else "с $from по $to"
+            filmYear.text = range
+        }
+
 
 
     }
